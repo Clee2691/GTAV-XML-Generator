3770a23d69fc540963bdab6ab5ba4bc571b888f2
Clee2691 <clee2691@gmail.com>
2020-05-23 11:39:23 -0400

    Initial commit

---
+++ xml_funcs.py
@@ -0,0 +1,12 @@
+import xml.etree.ElementTree as ET
+
+"""
+- Slot number with weapon entry
+- 2 SlotNavigateOrder
+- 1 SlotBestOrder
+
+<Item>
+    <OrderNumber value="10" />
+    <Entry>SLOT_[weapon_name]</Entry>
+</Item>
+"""
